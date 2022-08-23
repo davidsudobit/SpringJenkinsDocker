627a1949b9ef77a52439189b684d0d0a8bfeb4c1
davidsudobit <davidmanson474@gmail.com>
2022-08-24 02:14:05 +0530

    2nd commit

---
+++ src/main/java/com/SpringJenkinsDocker/Controller/GetName.java
@@ -0,0 +1,15 @@
+package com.SpringJenkinsDocker.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("name")
+public class GetName {
+	
+	@GetMapping("getName")
+	public String getName() {
+		return "david";
+	}
+}
